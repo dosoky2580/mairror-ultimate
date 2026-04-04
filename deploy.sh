9294add496f1685cd973ea501acf9b74adf1d6df
Dosoky2580 <dosoky2580@gmail.com>
2026-04-04 16:58:11 +0200

    Mirror Ultimate: Force Legacy Sync for Build Success

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "Mirror Ultimate: Retro-Compatible Dependencies Fix - Guaranteed Build Success"
+git commit -m "Mirror Ultimate: Force Legacy Sync for Build Success"
 git push origin main
