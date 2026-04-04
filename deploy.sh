982ab0d3ffce049b1cf7ace947e8599a5adaad2d
Dosoky2580 <dosoky2580@gmail.com>
2026-04-04 16:07:16 +0200

    Mirror Ultimate: Fixing Android Namespace and Gradle Sync - Final Build Attempt

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "Mirror Ultimate: Final Stability Fix - Custom Engine & Security Core"
+git commit -m "Mirror Ultimate: Fixing Android Namespace and Gradle Sync - Final Build Attempt"
 git push origin main
