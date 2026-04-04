f25f52c20cb24230f5e5a7835e05566fe704c912
Dosoky2580 <dosoky2580@gmail.com>
2026-04-04 15:28:50 +0200

    Mirror Ultimate: Merging Adham Brain, A4 Logic, and Offline Security Vault

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "Mirror Ultimate: Force Update for Security and Logic Hub"
+git commit -m "Mirror Ultimate: Merging Adham Brain, A4 Logic, and Offline Security Vault"
 git push origin main
