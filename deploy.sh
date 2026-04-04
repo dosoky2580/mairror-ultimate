cad38af055b4658c0b1987cc76e846cfa82b32b1
Dosoky2580 <dosoky2580@gmail.com>
2026-04-04 12:36:10 +0200

    Mirror Ultimate: Critical Update - Migrating file_picker to V2 Embedding

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
-git commit -m "Mirror Ultimate: Resolution of dependency conflicts - Following Server Mandate"
+git commit -m "Mirror Ultimate: Critical Update - Migrating file_picker to V2 Embedding"
 git push origin main
